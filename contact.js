758822fbf710bf3d9feec0a5725d2a002942ac9e
VictorCasas-no <victor.casas@enti.cat>
2025-03-28 14:17:37 +0100

    28/03 2

--- contact.js
+++ contact.js
@@ -33,7 +33,51 @@ document.addEventListener("DOMContentLoaded", (event) => {
 	
 	contact_form.addEventListener("submit", (event) => {
 		
-		alert("Enviado");
+		let send = true;
+		
+		if (nombre_field.value.length <= 2 || nombre_field.value.length > 32) {
+			
+			send = false;
+			nombre_field.style.border = "2px solid red";
+			
+		}
+		else {
+			nombre_field.style.border = "2px solid green";
+		}
+		
+		
+		console.log(nacimiento_field.value);
+		if (nacimiento_field.value == "") {
+			send = false;
+			nacimiento_field.style.border = "2px solid red";
+		}
+		else {
+			//Formato: 2025-03-28
+			let date = nacimiento_field.value.split("-");
+			
+			let year = parseInt(date[0]);
+			let month = parseInt(date[1]);
+			let day = parseInt(date[2]);
+			
+			if (year <= 0 || year > 2025) {
+				send = false;
+				nacimiento_field.style.border = "2px solid red";
+			}
+			if (month <= 0 || month > 12) {
+				send = false;
+				nacimiento_field.style.border = "2px solid red";
+			}
+			if (day <= 0 || day > 31) {
+				send = false;
+				nacimiento_field.style.border = "2px solid red";
+			}
+		
+		
+		if (!send) {
+			
+			event.preventDefault();
+		
+		}
 	
 	});
   
